65c687e9cb093e0b3e5b632c55ca2080875bdeab
Chase64 <103872287+Chase64@users.noreply.github.com>
2022-06-01 11:32:44 -0600

    messing around with if and else statements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-hi 
+var condition = 4;
+var firstName = "Bob";
+
+if (condition == 1) {
+    console.log("Hello, ", firstName, "!")
+} else if (condition <= 3) {
+    console.log("Hello, friend!");   
+} else {
+    console.log("Hello, mom!");
+}
+    
